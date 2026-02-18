61ba83a42fa942c48942d9b1aa2391b4087303b5
Jebeza <jbenaventzapata@gmail.com>
2026-02-18 10:46:33 +0100

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 export function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center py-8">
+      <div className="grid grid-cols-4 grid-rows-3 gap-4 w-full h-215 py-7 px-63.75">
+        
+        <div className="col-span-2 bg-red-400 rounded-[32px]">
+          1
+        </div>
+
+        <div className="col-start-3 bg-red-400 rounded-[32px]">
+          2
+        </div>
+
+        <div className="row-span-2 col-start-4 bg-red-400 rounded-[32px]">
+          3
+        </div>
+
+        <div className="row-start-2 bg-red-400 rounded-[32px]">
+          4
+        </div>
+
+        <div className="row-start-2 bg-red-400 rounded-[32px]">
+          5
+        </div>
+
+        <div className="row-span-2 row-start-2 bg-red-400 rounded-[32px]">
+          6
+        </div>
+
+        <div className="col-span-2 row-start-3 bg-red-400 rounded-[32px]">
+          7
+        </div>
+
+        <div className="col-start-4 row-start-3 bg-red-400 rounded-[32px]">
+          8
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
