bca7519b570679f97fcd6ad653887fa5b67b55ce
jebeza <jbenaventzapata@gmail.com>
2026-03-03 20:57:35 +0100

    Build responsive layout for sm and xs breakpoints. Format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,13 @@ const techStack = [
 
 function TechGrid() {
   return (
-    <div className="grid grid-cols-4 gap-9 place-items-center w-full">
+    <div className="grid grid-cols-4 gap-6 md:gap-9 place-items-center w-full">
       {techStack.map(({ name, icon: Icon }) => (
         <div
           key={name}
           className="flex flex-col items-center gap-2 hover:scale-110 transition-transform"
         >
-          <Icon className="text-gray-800 dark:text-[#b8a0ff] xl:text-[60px] md:text-[40px] lg:text-[40px]" />
+          <Icon className="text-[36px] md:text-[40px] xl:text-[60px] text-gray-800 dark:text-[#b8a0ff]" />
         </div>
       ))}
     </div>
@@ -53,63 +53,84 @@ export default function App() {
     <main className="font-body min-h-screen bg-[#f3efef] dark:bg-[#1b172a] px-6 md:px-12 py-10 text-black dark:text-[#e1d9ff]">
       {/* Header */}
       <header className="flex items-center justify-between mb-10 max-w-350 mx-auto">
-        <AuroraText className="text-3xl font-display">Jenifer</AuroraText>
+        <AuroraText className="text-xl md:text-2xl xl:text-3xl font-display">
+          Jenifer
+        </AuroraText>
       </header>
+
       <section
         className="
-          grid gap-6 justify-center mx-auto
-          sm:grid-cols-[repeat(4,180px)] sm:auto-rows-[180px]
+          grid gap-4 justify-center mx-auto
+          grid-cols-[repeat(2,164px)]
+          auto-rows-[164px]
+
           md:grid-cols-[repeat(4,180px)] md:auto-rows-[180px]
           lg:grid-cols-[repeat(4,180px)] lg:auto-rows-[180px]
           xl:grid-cols-[repeat(4,280px)] xl:auto-rows-[280px]
         "
       >
-        {/* 1 — About */}
+        {/* ABOUT */}
         <Card
           className="
+            col-span-2 row-span-2
             rounded-3xl bg-white dark:bg-[#2c2540] shadow-none
-            sm:col-span-2 sm:row-span-1
+
             md:col-span-2 md:row-span-2
             lg:col-span-2 lg:row-span-2
             xl:col-start-1 xl:col-span-2 xl:row-start-1 xl:row-span-1
           "
         >
           <CardContent className="flex flex-col justify-between h-full">
-            <div>
-              <img
-                src="/avatar.png"
-                alt="Jenifer's avatar"
-                className="w-25 h-30 border-0 dark:border-[#b8a0ff]"
-              />
-            </div>
-            <div>
-              <p className="leading-7 letter-spacing-[0.5px] font-body text-lg">
-                {strings.im}{" "}
-                <span className="text-4xl font-display dark:text-[#8f6fc4]">
-                  Jenifer
-                </span>
-                {strings.about}
-              </p>
-            </div>
-          </CardContent>{" "}
+            <img
+              src="/avatar.png"
+              alt="Jenifer's avatar"
+              className="
+                w-25 h-30
+                md:w-29 md:h-35
+                xl:w-25 xl:h-30
+              "
+            />
+
+            <p
+              className="
+                text-lg    
+                md:text-lg  
+                xl:text-lg
+                leading-relaxed
+              "
+            >
+              {strings.im}{" "}
+              <span
+                className="
+                  text-3xl     
+                  md:text-2xl   
+                  xl:text-4xl
+                  font-display
+                  dark:text-[#8f6fc4]
+                "
+              >
+                Jenifer
+              </span>
+              {strings.about}
+            </p>
+          </CardContent>
         </Card>
 
-        {/* 2 — Language */}
+        {/* LANGUAGE */}
         <Card
           className="
             rounded-3xl bg-white dark:bg-[#2c2540] shadow-none flex items-center justify-center
-            sm:col-span-2 sm:row-span-1
             md:col-span-1 md:row-span-1
             lg:col-span-1 lg:row-span-1
             xl:col-start-3 xl:row-start-1 xl:row-span-1
           "
         >
           <CardContent className="text-center">
-            <p className="xl:text-8xl md:text-7xl lg:text-7xl font-display dark:text-[#b8a0ff]">
+            <p className="text-6xl md:text-6xl xl:text-8xl font-display dark:text-[#b8a0ff]">
               {language.toUpperCase()}
             </p>
 
-            <div className="xl:text-sm md:text-xs lg:text-xs mt-2 flex gap-6 justify-center">
+            <div className="text-sm md:text-sm mt-2 flex gap-4 justify-center">
               {AVAILABLE_LANGUAGES.map((lang) => {
                 const isActive = lang === language;
 
@@ -117,7 +138,7 @@ export default function App() {
                   <span
                     key={lang}
                     onClick={() => changeLanguage(lang)}
-                    className={`cursor-pointer text-xl ${
+                    className={`cursor-pointer text-base md:text-lg ${
                       isActive ? "font-semibold scale-125" : "opacity-50"
                     }`}
                   >
@@ -129,10 +150,11 @@ export default function App() {
           </CardContent>
         </Card>
 
-        {/* 3 — Github */}
+        {/* GITHUB */}
         <Card
           className="
-            rounded-3xl bg-[#62CBDB] dark:bg-[#3aa6b8] text-black dark:text-black shadow-none relative overflow-hidden
+            row-span-2
+            rounded-3xl bg-[#62CBDB] dark:bg-[#3aa6b8] shadow-none relative overflow-hidden
             md:col-span-1 md:row-span-2
             lg:col-span-1 lg:row-span-2
             xl:col-start-4 xl:row-start-1 xl:row-span-2
@@ -140,17 +162,18 @@ export default function App() {
         >
           <CardContent className="flex flex-col justify-between h-full">
             <div>
-              <h2 className="xl:text-3xl md:text-3xl lg:text-3xl font-bold font-display mb-7">
+              <h2 className="text-xl md:text-2xl xl:text-3xl font-bold font-display mb-4 md:mb-7">
                 Github
-              </h2>{" "}
-              <p className="xl:text-lg md:text-sm lg:text-sm">
+              </h2>
+              <p className="text-sm md:text-sm xl:text-lg">
                 {strings.githubDesc}
               </p>
             </div>
+
             <Button
               size="icon"
               variant="secondary"
-              className="rounded-full w-10 h-10 absolute bottom-6 left-6 cursor-pointer z-10"
+              className="rounded-full w-10 h-10 absolute bottom-6 left-6 z-10"
             >
               <a href="https://github.com/jebeza" target="_blank">
                 <ArrowUpRight size={18} />
@@ -160,20 +183,28 @@ export default function App() {
           <div
             className="
               absolute
-              xl:-bottom-30 xl:-right-60
+              -bottom-28 -right-40
+              w-72 h-72
+
               md:-bottom-34 md:-right-65
-              lg:-bottom-34 lg:-right-65
-              xl:w-122 xl:h-98.25
               md:w-100 md:h-80
+
+              lg:-bottom-34 lg:-right-65
               lg:w-100 lg:h-80
-              bg-[#FAC2D7] dark:bg-[#a56fa5] rounded-full
+
+              xl:-bottom-30 xl:-right-60
+              xl:w-122 xl:h-98.25
+
+              bg-[#FAC2D7] dark:bg-[#a56fa5]
+              rounded-full
             "
           />{" "}
         </Card>
 
-        {/* 4 — Projects */}
+        {/* PROJECTS */}
         <Card
           className="
+            row-span-2
             rounded-3xl bg-[#FFA498] dark:bg-[#c47791] shadow-none relative overflow-hidden
             md:col-span-1 md:row-span-2
             lg:col-span-1 lg:row-span-2
@@ -182,18 +213,19 @@ export default function App() {
         >
           <CardContent className="flex flex-col justify-end h-full z-10">
             <div>
-              <h2 className="xl:text-3xl md:text-3xl lg:text-3xlfont-bold font-display mb-7">
+              <h2 className="text-xl md:text-2xl xl:text-3xl font-bold font-display mb-4 md:mb-7">
                 {strings.projectsTitle}
               </h2>
 
-              <p className="xl:text-lg md:text-sm lg:text-sm mb-12">
+              <p className="text-sm md:text-sm xl:text-lg mb-8 md:mb-12">
                 {strings.projectsDesc}
               </p>
             </div>
+
             <Button
               size="icon"
               variant="secondary"
-              className="rounded-full w-10 h-10 cursor-pointer"
+              className="rounded-full w-10 h-10"
             >
               <ArrowUpRight size={18} />
             </Button>
@@ -201,71 +233,50 @@ export default function App() {
           <div
             className="
               absolute
-              -top-40 -right-40
+              -top-35 -right-38
+              w-72 h-80
+
               md:-top-45 md:-right-40
-              lg:-top-45 lg:-right-40
-              xl:-top-90 xl:-right-90
-              w-80 h-96
               md:w-80 md:h-90
+
+              lg:-top-45 lg:-right-40
               lg:w-80 lg:h-90
+
+              xl:-top-90 xl:-right-90
               xl:w-150.75 xl:h-165.5
+
               bg-[#FAC2D7] dark:bg-[#5b4c8a]
               rounded-full
             "
-          />
+          />{" "}
         </Card>
 
-        {/* 5 — Email */}
-        <Card
-          className="
-            rounded-3xl bg-white dark:bg-[#2c2540] shadow-none flex items-center justify-center p-6
-            md:col-span-1 md:row-span-1
-            lg:col-span-1 lg:row-span-1
-            xl:col-start-1 xl:row-start-2 xl:row-span-1
-          "
-        >
-          <a
-            href="mailto:jbenaventzapata@gmail.com"
-            target="_blank"
-            className="flex items-center justify-center"
-          >
-            <SiMailboxdotorg className="text-[#62CBDB] dark:text-[#3aa6b8] xl:text-[90px] md:text-[60px] lg:text-[60px]" />
-          </a>{" "}
+        {/* EMAIL */}
+        <Card className="rounded-3xl bg-white dark:bg-[#2c2540] shadow-none flex items-center justify-center p-6">
+          <a href="mailto:jbenaventzapata@gmail.com" target="_blank">
+            <SiMailboxdotorg className="text-[54px] md:text-[60px] xl:text-[90px] text-[#62CBDB] dark:text-[#3aa6b8]" />
+          </a>
         </Card>
 
-        {/* 6 — LinkedIn */}
-        <Card
-          className="
-            rounded-3xl bg-[#FFA498] dark:bg-[#8f6fc4] shadow-none flex items-center justify-center
-            md:col-span-1 md:row-span-1
-            lg:col-span-1 lg:row-span-1
-            xl:col-start-2 xl:row-start-2 xl:row-span-1
-          "
-        >
+        {/* LINKEDIN */}
+        <Card className="rounded-3xl bg-[#FFA498] dark:bg-[#8f6fc4] shadow-none flex items-center justify-center">
           <a
             href="https://www.linkedin.com/in/jenifer-benavent-zapata"
             target="_blank"
-            className="text-5xl font-bold text-white"
           >
-            <SiLinkedin className="xl:text-[90px] md:text-[60px] lg:text-[60px]" />
-          </a>{" "}
+            <SiLinkedin className="text-[54px] md:text-[60px] xl:text-[90px] text-white" />
+          </a>
         </Card>
 
-        {/* 8 — Dark mode */}
-        <Card
-          className="
-            rounded-3xl bg-white dark:bg-[#2c2540] shadow-none flex items-center justify-center
-            md:col-span-1 md:row-span-1
-            lg:col-span-1 lg:row-span-1
-            xl:col-start-4 xl:row-start-3
-          "
-        >
-          <AnimatedThemeToggler className="cursor-pointer" />
+        {/* THEME */}
+        <Card className="rounded-3xl bg-white dark:bg-[#2c2540] shadow-none flex items-center justify-center">
+          <AnimatedThemeToggler />
         </Card>
 
-        {/* 7 — Tech stack */}
+        {/* TECH STACK */}
         <Card
           className="
+            col-span-2
             rounded-3xl bg-white dark:bg-[#2c2540] shadow-none p-6 flex items-center justify-center
             md:col-span-2 md:row-span-1
             lg:col-span-2 lg:row-span-1
